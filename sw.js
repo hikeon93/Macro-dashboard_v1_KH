473f3ef52adf97288fb571ef487a8234d8715992
hikeon93 <essboard367@naver.com>
2026-07-12 20:17:50 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "credit-valve-v1";
+const CACHE = "credit-valve-v2";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
